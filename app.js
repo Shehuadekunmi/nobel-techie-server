f32f3dd568da013d3c9d0c9b74716375dd6d425c
ShehuAdekunmi <sirkumyemo@gmail.com>
2025-04-06 12:28:13 -0700

    changing of frontend url

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(xss());
 app.use(cookieParser());
 // app.use(cors());
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://noble-techie.netlify.app'], 
+  origin: ['http://localhost:5173', 'https://nobel-tech.netlify.app'], 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
